3bf137f635f9a1afb2c52f5758bf6ab471fc28b4
Edith Thakkar <egarcia262@cnm.edu>
2018-04-22 09:10:01 -0600

    accessor and mutators for like

--- php/classes/like.php
+++ php/classes/like.php
@@ -50,3 +50,61 @@ class Like implements \JsonSerializable {
 		}
 	}
 }
+
+	/**
+	 *accessor method for like ProfileId
+	 *
+	 *@return Uuid value of Like profileId
+	 */
+	public function getLikeProfileId() : Uuid {
+		return($this->likeProfileId);
+	}
+
+	/**
+	 *mutator method for like ProfileId
+	 *
+	 *@param Uuid|string $newLikeProfileId new value of like ProfileId
+	 *@throws \InvalidArgumentException if $newLikeProfileId is not a st
+	 *@throws \RangeException if $newLikeProfileId is not positive
+	 * @throws \TypeError if $newLikeProfileId is not a uuid or str:
+	 */
+	public function setProfileId ( $newLikeProfileId) : void {
+		try {
+			$uuid = self::validateUuid($newLikeProfileId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception-getMessage(), 0, $exception));
+		}
+
+		//convert and store the like ProfileId
+		$this->likeProfileId = $uuid;
+	}
+
+	/**
+	 *accessor method for like PostId
+	 *
+	 *@return Uuid value of like PostId
+	 */
+	public function getLikePostId() : Uuid {
+		return($this->likeProfileId);
+	}
+
+	/**
+	 *mutator method for like PostId
+	 *
+	 *@param Uuid|string $newLikePostId new value of like PostId
+	 *@throws \InvalidArgumentException if $newLikePostId is not a st
+	 *@throws \RangeException if $newLikePostId is not positive
+	 * @throws \TypeError if $newLikePostId is not a uuid or str:
+	 */
+	public function setLikePostId ( $newLikePostId) : void {
+		try {
+			$uuid = self::validateUuid($newLikePostId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception-getMessage(), 0, $exception));
+		}
+
+		//convert and store the like PostId
+		$this->likeProfileId = $uuid;
+	}
