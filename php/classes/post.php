2f1f28f5952dc942aecddd6cae6eb453ab0e8007
Edith Thakkar <egarcia262@cnm.edu>
2018-04-22 08:44:46 -0600

    accessor and mutators

--- php/classes/post.php
+++ php/classes/post.php
@@ -77,3 +77,147 @@ class Post implements \JsonSerializable {
 	}
 }
 
+/**
+ *accessor method for post Id
+ *
+ *@return Uuid value of post Id
+ */
+public function getPostId() : Uuid {
+	return($this->postId);
+}
+
+/**
+ *mutator method for post Id
+ *
+ *@param Uuid|string $newPostId new value of post Id
+ *@throws \InvalidArgumentException if $newPostId is not a st
+ *@throws \RangeException if $newPostId is not positive
+ * @throws \TypeError if $newPostId is not a uuid or str:
+ */
+public function setPostId ( $newPostId) : void {
+	try {
+		$uuid = self::validateUuid($newPostId);
+	} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		$exceptionType = get_class($exception);
+		throw(new $exceptionType($exception-getMessage(), 0, $exception));
+	}
+
+	//convert and store the postId
+	$this->postId = $uuid;
+}
+
+/**
+ *accessor method for postImage
+ *
+ *@return Uuid value of post Image
+ */
+public function getPostImage() : Uuid {
+	return($this->postImage);
+}
+
+/**
+ *mutator method for post Image
+ *
+ *@param Uuid|string $newPostImage new value of post Image
+ *@throws \InvalidArgumentException if $newPostImage is not a st
+ *@throws \RangeException if $newPostImage is not positive
+ * @throws \TypeError if $newPostImage is not a uuid or str:
+ */
+public function setPostImage ( $newPostImage) : void {
+	try {
+		$uuid = self::validateUuid($newPostImage);
+	} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		$exceptionType = get_class($exception);
+		throw(new $exceptionType($exception-getMessage(), 0, $exception));
+	}
+
+	//convert and store the post Image
+	$this->postImage = $uuid;
+}
+
+/**
+ *accessor method for post Description
+ *
+ *@return Uuid value of post Description
+ */
+public function getPostDescription() : Uuid {
+	return($this->postDescription);
+}
+
+/**
+ *mutator method for post Description
+ *
+ *@param Uuid|string $newPostDescription new value of post Description
+ *@throws \InvalidArgumentException if $newPostDescription is not a st
+ *@throws \RangeException if $newPostDescription is not positive
+ * @throws \TypeError if $newPostDescription is not a uuid or str:
+ */
+public function setPostDescription ($newPostDescription) : void {
+	try {
+		$uuid = self::validateUuid($newPostDescription);
+	} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		$exceptionType = get_class($exception);
+		throw(new $exceptionType($exception-getMessage(), 0, $exception));
+	}
+
+	//convert and store the post Description
+	$this->post Drescription = $uuid;
+}
+
+/**
+ *accessor method for post Title
+ *
+ *@return Uuid value of post Title
+ */
+public function getPostTitle() : Uuid {
+	return($this->postTitle;
+}
+
+/**
+ *mutator method for post Title
+ *
+ *@param Uuid|string $newPostTitle new value of post Title
+ *@throws \InvalidArgumentException if $newPostTitle is not a st
+ *@throws \RangeException if $newPostTitle is not positive
+ * @throws \TypeError if $newPostTitle is not a uuid or str:
+ */
+public function setPostTitle ( $newPostTitle) : void {
+	try {
+		$uuid = self::validateUuid($newPostTitle);
+	} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		$exceptionType = get_class($exception);
+		throw(new $exceptionType($exception-getMessage(), 0, $exception));
+	}
+
+	//convert and store the post Title
+	$this->postTitle = $uuid;
+}
+
+/**
+ *accessor method for postProfileId
+ *
+ *@return Uuid value of post ProfileId
+ */
+public function getPostProfileId() : Uuid {
+	return($this->postProfileId);
+}
+
+/**
+ *mutator method for post ProfileId
+ *
+ *@param Uuid|string $newPostProfileId new value of post ProfileId
+ *@throws \InvalidArgumentException if $newPostProfileId is not a st
+ *@throws \RangeException if $newPostProfileId is not positive
+ * @throws \TypeError if $newPostProfileId is not a uuid or str:
+ */
+public function setPostProfileId ( $newPostProfileId) : void {
+	try {
+		$uuid = self::validateUuid($newPostProfileId);
+	} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+		$exceptionType = get_class($exception);
+		throw(new $exceptionType($exception-getMessage(), 0, $exception));
+	}
+
+	//convert and store the post ProfileId
+	$this->postPorfileId = $uuid;
+}
